eac3158b7c2c6ec4c9d64d6c469c7f622d418eb6
Grzegorz Sosidko <grzegorz.sosidko@gmail.com>
2022-02-06 23:50:41 +0100

    sry

---
+++ .biblioteka/NOWY_HEADER-TEN SKURWIEL/html/script.js
@@ -0,0 +1,12 @@
+
+
+var scene = document.getElementById('arrows-1');
+var parallaxInstance = new Parallax(scene, {
+    relativeInput: true
+});
+
+var scene1 = document.getElementById('arrows-2');
+var parallaxInstance1 = new Parallax(scene1, {
+    relativeInput: true
+});
+
